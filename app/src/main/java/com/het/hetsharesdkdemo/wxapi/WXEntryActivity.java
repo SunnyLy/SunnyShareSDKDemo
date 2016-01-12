65971555d259cb03c1fccb47053e62341bb007a4
SunnyLy <450512747@qq.com>
2016-01-12 17:06:13 +0800

    微信回调界面。。。。。。。

---
+++ app/src/main/java/com/het/hetsharesdkdemo/wxapi/WXEntryActivity.java
@@ -0,0 +1,55 @@
+package com.het.hetsharesdkdemo.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+/**
+ * Created by sunny on 2016/1/11.
+ * Annotion:
+ */
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+    private String WeiXinAppID = "";
+    private IWXAPI api;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, WeiXinAppID, true);
+        api.registerApp(WeiXinAppID);
+        api.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+        String result = "";
+        switch (resp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                result = "发送成功";
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                result = "发送取消";
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                result = "发送被拒绝";
+                break;
+            default:
+                result = "发送返回";
+                break;
+        }
+
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+        this.finish();
+    }
+}
