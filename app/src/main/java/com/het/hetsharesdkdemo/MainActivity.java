561e934076737678e6e525ca63b811fab93722cd
SunnyLy <450512747@qq.com>
2016-01-13 18:28:39 +0800

    ShareSDK 分享Model整理

--- app/src/main/java/com/het/hetsharesdkdemo/MainActivity.java
+++ app/src/main/java/com/het/hetsharesdkdemo/MainActivity.java
@@ -15,7 +15,9 @@ import com.het.share.dialog.CommonShareDialog;
 import com.het.share.listener.ICommonShareLinstener;
 import com.het.share.manager.CommonShareManager;
 import com.het.share.manager.CommonSharePlatform;
-import com.het.share.model.CommonShareBean;
+import com.het.share.model.CommonShareBaseBean;
+import com.het.share.model.CommonShareMusic;
+import com.het.share.model.CommonShareWebpage;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.UiError;
 
@@ -28,8 +30,8 @@ public class MainActivity extends AppCompatActivity implements ICommonShareLinst
     private String musicUrl = "http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3";
     private String musicDataUrl = "http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3";
     private String mTargetUrl = "http://www.baidu.com";
-    private String mTitle = "Sunny";
-    private String mDescription = "Hello,I'm Sunny";
+    private String mTitle = "阳总";
+    private String mDescription = "阳总,下午好！喝下午茶了！";
     private String mImgUrl = "http://pic15.nipic.com/20110725/7067632_020203324179_2.jpg";
 
     private String mWeixinAppId = "wxd930ea5d5a258f4f";//微信Demo用
@@ -95,58 +97,85 @@ public class MainActivity extends AppCompatActivity implements ICommonShareLinst
     @Override
     public void onStartShare(CommonSharePlatform sharePlatform) {
 
-        CommonShareBean shareBean = new CommonShareBean();
+        CommonShareBaseBean shareBean = new CommonShareBaseBean();
         shareBean.setUiListener(this);
         shareBean.setTitle(mTitle);
         shareBean.setDescription(mDescription);
         shareBean.setAppName("SunnyDemo");
         shareBean.setTargetUrl(mTargetUrl);
-        shareBean.setMusicUrl(musicUrl);
-        shareBean.setMusicDataUrl(musicDataUrl);
         shareBean.setBm(null);
         shareBean.setSharePlatform(sharePlatform);
 
+
+        /**
+         * 音乐
+         */
+        CommonShareMusic music = new CommonShareMusic();
+        music.setUiListener(this);
+        music.setTitle(mTitle);
+        music.setDescription(mDescription);
+        music.setAppName("SunnyDemo");
+        music.setTargetUrl(mTargetUrl);
+        music.setMusicUrl(musicUrl);
+        music.setMusicDataUrl(musicDataUrl);
+        music.setBm(null);
+        music.setSharePlatform(sharePlatform);
+
+
+        /**
+         * 网页
+         */
+        CommonShareWebpage webpage = new CommonShareWebpage();
+        webpage.setUiListener(this);
+        webpage.setTitle(mTitle);
+        webpage.setDescription(mDescription);
+        webpage.setAppName("SunnyDemo");
+        webpage.setTargetUrl(mTargetUrl);
+        webpage.setBm(null);
+        webpage.setSharePlatform(sharePlatform);
+
         //分享音乐
-        // mShareManger.shareMusic(shareBean);
+         mShareManger.shareMusic(music);
         //分享网页
-        shareBean.setImgUrl(mImgUrl);
-        mShareManger.shareWebpage(shareBean);
-        Toast.makeText(this,"点击："+sharePlatform,Toast.LENGTH_SHORT).show();
+       /* webpage.setImgUrl(mImgUrl);
+        mShareManger.shareWebpage(webpage);*/
+        Toast.makeText(this, "点击：" + sharePlatform, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onShareSuccess(CommonSharePlatform sharePlatform, String msg) {
 
-        Toast.makeText(this,"分享成功",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "分享成功", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onShareFialure(CommonSharePlatform sharePlatform, String msg) {
-        Toast.makeText(this,"分享失败",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "分享失败", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onShareCancel(CommonSharePlatform sharePlatform) {
-        Toast.makeText(this,"取消分享",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "取消分享", Toast.LENGTH_SHORT).show();
     }
 
     /**
      * //////////////下面是腾讯分享回调
+     *
      * @param o
      */
     @Override
     public void onComplete(Object o) {
-        Toast.makeText(this,"QQ分享完成",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "QQ分享完成", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onError(UiError uiError) {
-        Toast.makeText(this,"QQ分享onError",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "QQ分享onError", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onCancel() {
-        Toast.makeText(this,"QQ分享onCancel",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "QQ分享onCancel", Toast.LENGTH_SHORT).show();
     }
 
     @Override
